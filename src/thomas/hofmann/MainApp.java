651deda514565c315c21065ac771277d4254f371
Thomas <thomas.hofmann2003@gmail.com>
2019-04-24 22:42:34 +0200

    AI learns  to jump

--- src/thomas/hofmann/MainApp.java
+++ src/thomas/hofmann/MainApp.java
@@ -1,10 +1,13 @@
 package thomas.hofmann;
 
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -12,10 +15,25 @@ import javax.swing.JToggleButton;
 
 public class MainApp implements Engine,ActionListener
 {
+	Window main = null;
+	GraphicsEngine ge;
+	
+	NeuralNetwork nn;
+	
+	ArrayList<Double> input;
+	ArrayList<Double> output;
+	
+	Rectangle hitbox;
+	Rectangle obstacle;
+	double velocityY=0,time=1,gravity=0.1;
+	int obstacleX=900;
+	int hitboxY=0;
 	int frameRate=60;
+	int deathCounter=0;
 	JTextField frameRateInput;
 	JButton start;
 	boolean draw=true;
+	boolean jumping;
 	boolean running=false;
 	JToggleButton drawing;
 	int i;
@@ -42,12 +60,31 @@ public class MainApp implements Engine,ActionListener
 		controlPanel.add(drawing);
 		controls.add(controlPanel);
 		controls.validate();
+		
+		input = new ArrayList<>();
+		input.add(0.0);
+		nn = new NeuralNetwork();
+		nn.addInputLayer(1, ActFunctions.TANH);
+		nn.addHiddenLayer(4, ActFunctions.TANH);
+		nn.addOutputLayer(2, ActFunctions.RELU);
+		
+		nn.randomizeWeights(0, 1);
+		
+		hitbox = new Rectangle();
+		obstacle = new Rectangle();
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0)
 	{
-		
+		if(arg0.getKeyCode()==KeyEvent.VK_SPACE)
+		{
+			if(!jumping)
+			{
+				jumping=true;
+				velocityY=8;
+			}
+		}
 	}
 
 	@Override
@@ -103,17 +140,70 @@ public class MainApp implements Engine,ActionListener
 	{
 		
 	}
-
+	
 	@Override
 	public void draw(Graphics2D g)
 	{
-		g.fillRect(i, 0, 100, 100);
+		g.fill(obstacle);
+		g.draw(hitbox);
 	}
 
 	@Override
 	public void loop(GraphicsEngine ge)
 	{
-		i++;
+		input.set(0, (double) obstacleX);
+		output=nn.compute(input);
+		if(output.get(0)>output.get(1))
+		{
+			if(!jumping)
+			{
+				jumping=true;
+				velocityY=8;
+			}
+		}
+		
+		time +=0.02;
+		if(obstacleX<-100)
+		{
+			obstacleX=ge.width+100;
+		}
+		obstacleX-=5;
+		
+		hitboxY += velocityY * time;
+	    velocityY -= gravity * time;
+	    
+	    if(hitboxY<0)
+	    {
+	    	velocityY=0;
+	    	hitboxY=0;
+	    	time=1;
+	    	jumping=false;
+	    }
+	    
+	    hitbox.setBounds(10, hitboxY, 75, 75);
+	    obstacle.setBounds(obstacleX, 0, 100, 100);
+	    
+		if(hitbox.intersects(obstacle))
+		{
+			obstacleX=900;
+			time=1;
+			hitboxY=0;
+			velocityY=0;
+			jumping=false;
+			nn.mutateWeights(0.1);
+			nn.mutateBias(0.1);
+			if(deathCounter>250)
+			{
+				nn.randomizeWeights(-1, 5);
+				deathCounter=0;
+			}
+			deathCounter++;
+			if(draw)
+			{
+				System.out.println(deathCounter);
+			}
+		}
+	    
 		if(draw)
 		{
 			ge.redraw();
@@ -132,11 +222,20 @@ public class MainApp implements Engine,ActionListener
 		{
 			if(!running)
 			{
-				Window main = new Window("JumpAndLearn", 1000, 600, true);
-				GraphicsEngine ge = new GraphicsEngine(main, this);
+				main = new Window("JumpAndLearn", 1000, 600, true);
+				ge = new GraphicsEngine(main, this);
 				frameRate=Integer.valueOf(frameRateInput.getText()).intValue();
 				ge.start();
 				running=true;
+				start.setText("End");
+			}else
+			{
+				
+				obstacleX=900;
+				this.main.dispose();
+				start.setText("Start");
+				running=false;
+				ge.stop();
 			}
 		}
 		if(e.getSource()==drawing)
